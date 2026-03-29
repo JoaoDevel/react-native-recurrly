0d782b0daea9f265c10f485add895f24fe445d30
JoãoDevel <joaovitormatosfontes15@gmail.com>
2026-03-29 21:10:28 +0100

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function SignIn() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Sign In</Text>
+      <Link href="/(auth)/sign-up">Create Account</Link>
+    </View>
+  );
+}
